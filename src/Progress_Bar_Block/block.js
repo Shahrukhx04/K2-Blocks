6483d6dd51e8e4de90a1f3ffad2812de895c18f6
shahrukhx04 <shahrukhx04@gmail.com>
2020-04-23 22:51:22 +0500

    Progress Bar With Animation and style options

--- src/Progress_Bar_Block/block.js
+++ src/Progress_Bar_Block/block.js
@@ -13,7 +13,7 @@ import './style.scss';
 
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType,
-      // For attribute sources
+	// For attribute sources
 } = wp.blocks;
 const {
 	RichText,
@@ -26,7 +26,9 @@ const {
 	PanelRow,
 	TextControl,
 	RangeControl,
-	ColorPicker
+	ColorPicker,
+	ToggleControl,
+	SelectControl
 
 } = wp.components;
 
@@ -59,10 +61,6 @@ registerBlockType( 'cgb/progressbar-block', {
 
 		progressBarColor: {
 			type: 'string',
-			default: 'yellow'
-		},
-		progressBorderColor: {
-			type: 'string',
 			default: 'red'
 		},
 		progressBarHeight: {
@@ -75,12 +73,45 @@ registerBlockType( 'cgb/progressbar-block', {
 		},
 		titleColor: {
 			type: 'string',
-			default: 'Green'
+			default: 'red'
 		},
 		progressBarPercentage:{
 			type: 'number',
-			default: 20
+			default: 40
+		},
+		ShowPercentage: {
+			type: 'boolean',
+			default: true
+		},
+		TextFontSize: {
+			type: 'number',
+			default: 1
+		},
+		ProgressBarBackGroundColor: {
+			type: 'string',
+			default: 'lightgray'
+		},
+		AnimateProgressBar: {
+			type: 'boolean',
+			default: true
+		},
+		AnimationState: {
+			type: 'string',
+			default: 'running'
+		},
+		TextFontFamily: {
+			type: 'string',
+			default: 'Lucida Console'
+		},
+		TextFontWeight: {
+			type: 'string',
+			default: 'normal'
+		},
+		ProgressBarBorderRadius: {
+			type: 'number',
+			default: 30
 		}
+
 	},
 
 
@@ -103,33 +134,60 @@ registerBlockType( 'cgb/progressbar-block', {
 		const {titleColor,title,borderColor,progressBarColor} = attributes
 
 		const ToolBarColors = [
-			{
-				color: 'red'
-			},
-			{
-				color: 'green'
-			},
-			{
-				color: 'blue'
-			},
-			{
-				color: 'orange'
-			},
-			{
-				color: 'yellow'
-			}
+			{ color: 'red' },
+			{ color: 'green' },
+			{ color: 'blue' },
+			{ color: 'orange' },
+			{ color: 'yellow' }
 		];
-		const AnimatedBarStyling = {
-			backgroundColor: progressBarColor,
-			animation: 'AnimateProgressBar 5s ease-in-out'
+
+		const FontsAvalaible = [
+			{ label: 'Lucida Console'},
+			{ label: 'Courier'},
+			{ label: 'monospace'},
+			{ label: 'Arial'}
+		]
+
+		const FontWeightAvaibles= [
+			{ label: 'normal'},
+			{ label: '100'},
+			{ label: '200'},
+			{ label: '300'},
+			{ label: '400'},
+			{ label: '500'},
+			{ label: '600'},
+		]
+
+		const ProgressBarOutsideContainerStyling = {
+			backgroundColor: attributes.ProgressBarBackGroundColor,
+			height: attributes.progressBarHeight + "em",
+			borderRadius: attributes.ProgressBarBorderRadius + 'px'
 		}
 
+		const ProgressBarInsideAnimationStyling = {
+			width: attributes.progressBarPercentage + "%",
+			height: attributes.progressBarHeight + "em",
+			backgroundColor: attributes.progressBarColor,
+			borderRadius: attributes.ProgressBarBorderRadius + 'px'
+
+		}
+
+		const ProgressBarInsideAnimationSpanStyling = {
+			animationPlayState: attributes.AnimationState
+		}
 
 		const BarOutlineStyling = {
 			borderColor: attributes.progressBorderColor,
 			height: attributes.progressBarHeight + 'em'
 		}
 
+		const TextStyling = {
+			fontSize: attributes.TextFontSize + 'em',
+			color: attributes.titleColor,
+			align: 'left',
+			fontFamily: attributes.TextFontFamily,
+			fontWeight: attributes.TextFontWeight
+		}
 		function onTitleColorChange(NewColor) {
 
 			setAttributes(
@@ -170,70 +228,221 @@ registerBlockType( 'cgb/progressbar-block', {
 			})
 		}
 
+		function onGradient1ColorChange(Newcolor) {
+			setAttributes({
+				ProgressBarGradientColor1: Newcolor
+			})
+		}
+
+		function onGradient2ColorChange(NewColor) {
+			setAttributes({
+				ProgressBargradientColor2: NewColor
+			})
+		}
+
+		function onShowPercentageToggleChange(NewValue) {
+			setAttributes({
+				ShowPercentage: NewValue
+			})
+		}
+
+		function onTextFontSizeChange(NewFont) {
+			setAttributes({
+				TextFontSize: NewFont
+			})
+		}
+
+		function onAnimateProgressBar(Animate) {
+			setAttributes({
+				AnimateProgressBar: Animate,
+			})
+
+			if (attributes.AnimationState == 'paused'){
+				setAttributes({
+					AnimationState: 'running'
+				})
+			} else {
+				setAttributes({
+					AnimationState: 'paused'
+				})
+			}
+
+		}
+
+		function onChangeProgressBarBackgound(NewColor) {
+			setAttributes({
+				ProgressBarBackGroundColor: NewColor
+			})
+		}
+
+		function onTextFontChange(NewFont) {
+			setAttributes({
+				TextFontFamily:  NewFont
+			})
+		}
+
+		function onFontWeightChange(NewWeight) {
+			setAttributes({
+				TextFontWeight: NewWeight
+			})
+		}
+
+		function onChangeProgressBarBorderRadius(NewBorders) {
+			setAttributes({
+				ProgressBarBorderRadius: NewBorders
+			})
+		}
 		return ([
 
 				<InspectorControls>
+					<PanelBody>
+
+						<RangeControl
+							label={<strong> Progress Bar Height </strong>}
+							value={ attributes.progressBarHeight }
+							onChange={ onBarHeightChange }
+							min={ 1 }
+							max={ 6 }
+							step ={0.1}
+						/>
+
+
+						<RangeControl
+							label={<strong>Progress</strong>}
+							value={ attributes.progressBarPercentage }
+							onChange={ OnBarPercentageChange }
+							min={ 0 }
+							max={ 100 }
+						/>
+					</PanelBody>
+
+
 					<PanelBody title={"Colors"}>
 
 						<p><strong>Title Color</strong></p>
-							<ColorPalette
-								value = { titleColor }
-								onChange={onTitleColorChange}
-								colors = {ToolBarColors}
-							/>
-							<p><strong>Progress Bar Color</strong></p>
-							<ColorPalette
-								value = {progressBarColor}
-								onChange = {onBarColorChange}
-								colors = {ToolBarColors} />
+						<ColorPalette
+							value = { titleColor }
+							onChange={onTitleColorChange}
+							colors = {ToolBarColors}
+						/>
+						<p><strong>Progress Bar Color</strong></p>
+						<ColorPalette
+							value = {attributes.progressBarColor}
+							onChange = {onBarColorChange}
+							colors = {ToolBarColors} />
 
-							<p><strong>Border Color</strong></p>
-							<ColorPalette
-								value={attributes.progressBorderColor}
-								onChange={onBorderColorChange}
-								colors = {ToolBarColors} />
 
 					</PanelBody>
 
 					<PanelBody title={'Text'}>
-							<TextControl
-								label={<strong>Title</strong>}
-								onChange={onTitleChange}
+						<TextControl
+							label={<strong>Title</strong>}
+							onChange={onTitleChange}
+							value = {attributes.title}
+						/>
+
+						<PanelRow>
+
+							<p>
+								Show Percentage
+							</p>
+							<ToggleControl
+								checked = {attributes.ShowPercentage}
+								onChange = {onShowPercentageToggleChange}
 							/>
 
+						</PanelRow>
+
+						<RangeControl
+							label={<p> <strong> Font Size </strong> </p>}
+							value={ attributes.TextFontSize }
+							onChange={ onTextFontSizeChange }
+							min={ 1 }
+							max={ 10 }
+							step ={0.1}
+						/>
+
+						<PanelRow>
+							<SelectControl
+								label="Font Family"
+								value={ attributes.TextFontFamily }
+								options={ FontsAvalaible }
+								onChange={ onTextFontChange}
+							/>
+
+						</PanelRow>
+
+						<PanelRow>
+							<SelectControl
+								label="Weight"
+								value={ attributes.TextFontWeight }
+								options={ FontWeightAvaibles }
+								onChange={ onFontWeightChange}
+							/>
+
+						</PanelRow>
 
 
 					</PanelBody>
 
-					<PanelBody title={'Other'}>
+					<PanelBody title={'Bar'}>
 
-							<RangeControl
-								label={<strong> Progress Bar Height </strong>}
-								value={ attributes.progressBarHeight }
-								onChange={ onBarHeightChange }
-								min={ 1 }
-								max={ 6 }
-								step ={0.1}
+						<PanelRow>
+							<p>
+								Animation
+							</p>
+							<ToggleControl
+								checked = {attributes.AnimateProgressBar}
+								onChange = {onAnimateProgressBar}
 							/>
 
+						</PanelRow>
 
-							<RangeControl
-								label={<strong>Progress</strong>}
-								value={ attributes.progressBarPercentage }
-								onChange={ OnBarPercentageChange }
-								min={ 0 }
-								max={ 100 }
-							/>
+						<RangeControl
+							label={<p> <strong> Border Raduis </strong> </p>}
+							value={ attributes.ProgressBarBorderRadius }
+							onChange={ onChangeProgressBarBorderRadius }
+							min={ 1 }
+							max={ 50 }
+							step ={1}
+						/>
+
 					</PanelBody>
+
+					<PanelBody title={'Background'}>
+
+						<PanelRow>
+							<p><strong>Title Color</strong></p>
+							<ColorPalette
+								value = { attributes.ProgressBarBackGroundColor }
+								onChange={onChangeProgressBarBackgound}
+								colors = {ToolBarColors}
+							/>
+
+						</PanelRow>
+
+					</PanelBody>
+
+
 
 				</InspectorControls>,
 
+				<div>
+					<p style={ TextStyling}>
+						{attributes.title}
+						{
+							(attributes.ShowPercentage == false) ?
+								''
+								: <span style={{float: 'right'}}> {attributes.progressBarPercentage} % </span>
+						}
 
-				<p style={{color: titleColor, align: 'left'}}>
-					{attributes.title}  {attributes.progressBarPercentage}%
-				</p>,
-				<div className="meter" style={BarOutlineStyling}>
-					<span style={ { width: attributes.progressBarPercentage + "%"} }><span className="progress" style={AnimatedBarStyling}></span></span>
+					</p>
+
+					<div style={ProgressBarOutsideContainerStyling} className="ProgressBarOutsideContainer">
+						<div className="ProgressBarInsideAnimation" style={ProgressBarInsideAnimationStyling}>
+							<span style={ProgressBarInsideAnimationSpanStyling}></span>
+						</div>
+					</div>
 				</div>
 
 			]
@@ -253,26 +462,53 @@ registerBlockType( 'cgb/progressbar-block', {
 	 */
 	save ({attributes}) {
 
-		const AnimatedBarStyling = {
-			backgroundColor: attributes.progressBarColor,
-			animation: 'AnimateProgressBar 5s ease-in-out'
+
+		const ProgressBarOutsideContainerStyling = {
+			backgroundColor: attributes.ProgressBarBackGroundColor,
+			height: attributes.progressBarHeight + "em",
 		}
 
+		const ProgressBarInsideAnimationStyling = {
+			width: attributes.progressBarPercentage + "%",
+			height: attributes.progressBarHeight + "em",
+			backgroundColor: attributes.progressBarColor,
+
+		}
+
+		const ProgressBarInsideAnimationSpanStyling = {
+			animationPlayState: attributes.AnimationState
+		}
 
 		const BarOutlineStyling = {
 			borderColor: attributes.progressBorderColor,
 			height: attributes.progressBarHeight + 'em'
 		}
 
-
+		const TextStyling = {
+			fontSize: attributes.TextFontSize + 'em',
+			color: attributes.titleColor,
+			align: 'left',
+			fontFamily: attributes.TextFontFamily,
+			fontWeight: attributes.TextFontWeight
+		}
 		return <div>
-			<p style={{color: 'Green'}}>
-				{attributes.title} {attributes.progressBarPercentage}%
+			<p style={ TextStyling}>
+				{attributes.title}
+				{
+					(attributes.ShowPercentage == false) ?
+						''
+						: <span style={{float: 'right'}}> {attributes.progressBarPercentage} % </span>
+				}
+
 			</p>
-			<div className="meter" style={BarOutlineStyling}>
-				<span style={ { width: attributes.progressBarPercentage + '%'} }><span className="progress" style={AnimatedBarStyling}></span></span>
+
+			<div style={ProgressBarOutsideContainerStyling} className="ProgressBarOutsideContainer">
+				<div className="ProgressBarInsideAnimation" style={ProgressBarInsideAnimationStyling}>
+					<span style={ProgressBarInsideAnimationSpanStyling}></span>
+				</div>
 			</div>
 		</div>
+
 
 
 	},

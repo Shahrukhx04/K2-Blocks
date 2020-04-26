1c379879c5ea69abd15ebe16a14ca988f217f788
Harry <syedharis632@gmail.com>
2020-04-26 18:25:30 +0500

    added time selector and shape option

--- src/Timer_Block/Timer.js
+++ src/Timer_Block/Timer.js
@@ -5,6 +5,24 @@ import './editor.scss';
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
 
+const {
+	RichText,
+	InspectorControls,
+	ColorPalette
+} = wp.editor;
+
+const {
+	PanelBody,
+	PanelRow,
+	TextControl,
+	RangeControl,
+	ColorPicker,
+	ToggleControl,
+	SelectControl,
+	DateTimePicker
+
+} = wp.components;
+
 /**
  * Register: aa Gutenberg Block.
  *
@@ -18,82 +36,6 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
  *                             registered; otherwise `undefined`.
  */
 
-
-class Timer extends wp.element.Component {
-	constructor(props) {
-	  super(props);
-	  this.target = new Date();
-	  this.state = {
-		seconds: 4,
-		minutes: 3,
-		hours: 2,
-		days: 1
-	  };
-	}
-
-
-	componentDidMount() {
-		this.intervalID = setInterval(
-		  () => this.tick(),
-		  1000
-		);
-	}
-	
-	componentWillUnmount() {
-		clearInterval(this.intervalID);
-	}
-
-	tick() {
-		var variable1 = new Date();
-		var variable2 = this.target;
-		// Create a third variable by adding both variables:
-		var delta = Math.abs((variable2-variable1)/1000);
-		// calculate (and subtract) whole days
-		var days_ = Math.floor(delta / 86400);
-		delta -= days_ * 86400;
-		// calculate (and subtract) whole hours
-		var hours_ = Math.floor(delta / 3600) % 24;
-		delta -= hours_ * 3600;
-		// calculate (and subtract) whole minutes
-		var minutes_ = Math.floor(delta / 60) % 60;
-		delta -= minutes_ * 60;
-		// what's left is seconds
-		var seconds_ = Math.floor(delta % 60);  // in theory the modulus is not required
-		this.setState({
-			seconds: seconds_,
-			minutes: minutes_,
-			hours: hours_,
-			days:days_
-		});
-	}
-
-	render() {
-		return (
-		<div class= "time-widget" style={{height:"auto"}}>
-			<div class="tw-row">
-				<div class="tw-column">
-					<p class="tw-digit tw-digit-days">{this.state.days}</p>
-					<p class="tw-title">days</p>
-				</div>
-				<div class="tw-column">
-					<p class="tw-digit tw-digit-hours">{this.state.hours}</p>
-					<p class="tw-title">hours</p>
-				</div>
-				<div class="tw-column">
-					<p class="tw-digit tw-digit-minutes">{this.state.minutes}</p>
-					<p class="tw-title">minutes</p>
-				</div>
-				<div class="tw-column">
-					<p class="tw-digit tw-digit-seconds">{this.state.seconds}</p>
-					<p class="tw-title">seconds</p>
-				</div>
-			  </div>
-			  </div>
-			)
-	}
-  } 
-
-
 registerBlockType( 'cgb/timer-block', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
 	title: 'Timer',
@@ -103,65 +45,135 @@ registerBlockType( 'cgb/timer-block', {
 		minutes: {type:'integer',default:45},
 		hours: {type:'integer',default:23},
 		date: {type:'integer',default:24},
-		month:{type:'integer',default:12},
+		month:{type:'integer',default:11},
 		year:{type:'integer',default:2020},
+		backgroundColor: {
+			type: 'string',
+			default: '#ddd'
+		},
+		timerShape: {
+			type: 'string',
+			default: ''
+		}
+
 	},
 
 	edit: function(props) {
-			var timer = new Timer();
 			console.log("Called here")
-			function updateContent(event) {
-				console.log("update content called");
-				if (event.target.name === 'timer_date_input'){
-					//create new date with new date and same time
-					var temp = event.target.value.split("/");
-					console.log(temp);
-					//target_date = new Date(/*year*/parseInt(temp[2]), parseInt(temp[0])-1/*month*/,parseInt(temp[1]) /*day*/, props.attributes.hours/*hours*/,props.attributes.minutes /*minutes*/, 0/*seconds*/, 0/*milliseconds*/);
-					if(temp.length == 3){
-						props.setAttributes({
-							minutes: props.attributes.minutes,
-							hours: props.attributes.hours,
-							date: parseInt(temp[1]),
-							month:parseInt(temp[0])-1,
-							year:parseInt(temp[2]),
-						})
-					}
-				}
-				else if (event.target.name === 'timer_time_input'){
-					//create new date with new time and old date
-					var temp = event.target.value.split(":");
-					//target_date = new Date(/*year*/props.attributes.year, props.attributes.month/*month*/,props.attributes.date /*day*/, parseInt(temp[0])/*hours*/, parseInt(temp[1])/*minutes*/, 0/*seconds*/, 0/*milliseconds*/);
-					if(temp.length == 2){
-						props.setAttributes({
-							minutes: parseInt(temp[1]),
-							hours: parseInt(temp[0]),
-							date: props.attributes.date,
-							month:props.attributes.month,
-							year:props.attributes.year,
-						})
-					}
-				}
-				timer.setTarget(props.attributes);
+
+			function updateDateTime(newDateTime){
+				//"2020-12-24T22:45:00"
+				var datetime = newDateTime.split('T');
+				var date = datetime[0].split('-');
+				var time = datetime[1].split(':');
+				console.log(date)
+				console.log(time)
+				props.setAttributes({
+					minutes: parseInt(time[1]),
+					hours: parseInt(time[0]),
+					date: parseInt(date[2]),
+					month: parseInt(date[1])-1,
+					year: parseInt(date[0]),
+				})
+			}
+
+			function onShapeChange(value){
+				props.setAttributes({
+					timerShape:value
+				})
+			}
+
+			function onBackgroundColorChange(value){
+
+			}
+
+			function onFontColorChange(value){
+				
 			}
 			
-			return (
-				<div id="timer-be">
-					<label>Select date:</label><br></br>
-					{React.createElement("input", { type: "text",id: "timer_date_input",name:"timer_date_input",placeholder:"MM/DD/YYYY",
-							defaultValue:props.attributes.month+"/"+props.attributes.date+"/"+props.attributes.year,  onChange: updateContent })}
-					<br></br>
-					<label>Select time:</label><br></br>
-					<input type="input" id="timer_time_input" name="timer_time_input" placeholder="HH-MM" defaultValue={props.attributes.hours+":"+props.attributes.minutes} onChange={updateContent}></input>
+			return ([
+				<InspectorControls>
+					<PanelBody title={"Date and Time"}>
+						<DateTimePicker
+							currentDate = {new Date(props.attributes.year,props.attributes.month,props.attributes.date,props.attributes.hours,props.attributes.minutes,0,0)}
+							onChange={updateDateTime}
+						/>
+					
+					</PanelBody>
+					<PanelBody title={"Styling and color"}>
+						<SelectControl
+									label="Shape"
+									value={props.attributes.timerShape}
+									options={[
+										{ label: 'Square', value: '' },
+										{ label: 'Round', value: 'tw-round' },
+									]}
+									onChange={onShapeChange}
+						/>
+						<ColorPalette
+							label="Background color"
+							value = { props.attributes.backgroundColor }
+							onChange={onBackgroundColorChange}
+							colors = {ToolBarColors}
+						/>
+						<ColorPalette
+							label="Font color"
+							value = { titleColor }
+							onChange={onFontColorChange}
+							colors = {ToolBarColors}
+						/>
+					</PanelBody>
+				</InspectorControls>
+				,
+				<div className="tw-holder">
+					<div class= "time-widget">
+						<div class="tw-row">
+							<div class={"tw-column"+" "+props.attributes.timerShape}>
+								<p class="tw-digit tw-digit-days">0</p>
+								<p class="tw-title">days</p>
+							</div>
+							<div class={"tw-column"+" "+props.attributes.timerShape}>
+								<p class="tw-digit tw-digit-hours">0</p>
+								<p class="tw-title">hours</p>
+							</div>
+							<div class={"tw-column"+" "+props.attributes.timerShape}>
+								<p class="tw-digit tw-digit-minutes">0</p>
+								<p class="tw-title">minutes</p>
+							</div>
+							<div class={"tw-column"+" "+props.attributes.timerShape}>
+								<p class="tw-digit tw-digit-seconds">0</p>
+								<p class="tw-title">seconds</p>
+							</div>
+						</div>
+					</div>
 				</div>
-			)
+			])
 		}
 	,
 	save: function(props) {
-		var timer = new Timer();
-		var timer_str = props.attributes.year+","+props.attributes.month+","+props.attributes.date+","+props.attributes.hours+","+props.attributes.minutes+","+0+","+0;
+	var timer_str = props.attributes.year+","+props.attributes.month+","+props.attributes.date+","+props.attributes.hours+","+props.attributes.minutes+","+0+","+0;
 	return (
 		<div className="tw-holder" data-time={timer_str}>
-		  {timer.render()}
+		  <div class= "time-widget">
+			<div class="tw-row">
+				<div class={"tw-column"+" "+props.attributes.timerShape}>
+					<p class="tw-digit tw-digit-days">0</p>
+					<p class="tw-title">days</p>
+				</div>
+				<div class={"tw-column"+" "+props.attributes.timerShape}>
+					<p class="tw-digit tw-digit-hours">0</p>
+					<p class="tw-title">hours</p>
+				</div>
+				<div class={"tw-column"+" "+props.attributes.timerShape}>
+					<p class="tw-digit tw-digit-minutes">0</p>
+					<p class="tw-title">minutes</p>
+				</div>
+				<div class={"tw-column"+" "+props.attributes.timerShape}>
+					<p class="tw-digit tw-digit-seconds">0</p>
+					<p class="tw-title">seconds</p>
+				</div>
+			  </div>
+			</div>
 		</div>
 	  );
 	},
